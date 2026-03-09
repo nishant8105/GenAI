68d519424cf6ddac5f40be563f759b3546d44ff8
nishant8105 <nishantjadhav8105@gmail.com>
2026-03-09 21:49:40 +0530

    Assignment 1 Task 1 for Gen AI

--- Assignment1/task1.py
+++ Assignment1/task1.py
@@ -1,12 +1,13 @@
-prodcuts = ["Samsung S22", "HP Victus", "Lenovo LOQ", "Iphone 17", "Samsung Book 5", "Oppo A7"]
-sample_prodcut = ("Samsung S22", 12999, "Phone")
+products = ["Laptop", "Headphones", "Keyboard", "Mouse", "Monitor", "Webcam"]
+sample_prodcut = ("Laptop", 12999, "Electronics")
 
 # print 2nd and last product from list
-print("2nd product",prodcuts[1], "last product", prodcuts[-1])
+print("2nd product",prodcuts[1])
+print("last product", prodcuts[-1])
 
 # append 2 prodcuts
-prodcuts.append("Realme 7")
-prodcuts.append("Samsung S26 Ultra")
+prodcuts.append("USB Hub")
+prodcuts.append("Desk Lamp")
 
 # print updated list
 print("Updated list", prodcuts)
@@ -15,3 +16,4 @@ print("Updated list", prodcuts)
 sample_prodcut = list(sample_prodcut)
 sample_prodcut[1] = 15999
 sample_prodcut = tuple(sample_prodcut)
+print("Updated tuple", sample_prodcut)
