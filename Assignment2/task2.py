e8b92b31e0e458c0bf560a891b408db5610af827
nishant8105 <nishantjadhav8105@gmail.com>
2026-03-12 19:23:25 +0530

    Task 2 complete

---
+++ Assignment2/task2.py
@@ -0,0 +1,19 @@
+orders = [1200, 2500, 800, 1750, 3000]
+
+revenue = 0
+for order in orders:
+    if order >= 2000:
+        final_amount = order - (order * 0.15)
+        print(f"{order} -> discount 15% -> {final_amount}")
+    elif (order >= 1500) and (order < 2000):
+        final_amount = order - (order * 0.10)
+        print(f"{order} -> discount 10% -> {final_amount}")
+    elif (order >= 1000) and (order < 1500) :
+        final_amount = order - (order * 0.07)
+        print(f"{order} -> discount 07% -> {final_amount}")
+    else :
+        print(f"{order}  -> discount 0% -> {final_amount}")
+
+    revenue += order
+
+print(f"Total revenue : {revenue}")
