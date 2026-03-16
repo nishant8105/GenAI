740ecfd6ce84f76790e81f8cdcc3a4d16944f920
nishant8105 <nishantjadhav8105@gmail.com>
2026-03-16 20:49:06 +0530

    update with file

--- Assignment4/task2.py
+++ Assignment4/task2.py
@@ -1,16 +1,22 @@
-f = open('sales_date2.txt', 'r')
+sales = [1200, 450, 980, 1500, 3000]
+f = open('sales_date.txt', 'w')
+for sale in sales:
+    f.write(f'{str(sale)}\n')
+f.close()
+
+f = open('sales_date.txt', 'r')
 print("Read entire file")
 print(f.read())
 f.close()
 
 print("-"*50)
-f = open('sales_date2.txt', 'r')
+f = open('sales_date.txt', 'r')
 print("Read first line")
 print(f.readline())
 f.close()
 
 ~print("-"*50)
-f = open('sales_date2.txt', 'r')
+f = open('sales_date.txt', 'r')
 print("Read all line using readlines")
 line = [line.strip() for line in f.readlines()]
 print(line)
