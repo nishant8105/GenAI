71a35b18cd638ba47ee22d4ef11fa96fb22eb7aa
nishant8105 <nishantjadhav8105@gmail.com>
2026-03-16 21:12:20 +0530

    Task 3 complete and correct file name in task 1 and 2 and rename file.

--- Assignment4/task1.py
+++ Assignment4/task1.py
@@ -1,18 +1,18 @@
 sales = [1200, 450, 980, 1500, 3000]
-f = open('sales_date.txt', 'w')
+f = open('sales_data.txt', 'w')
 for sale in sales:
     f.write(f'{str(sale)}\n')
 f.close()
 
-f = open('sales_date.txt', 'r')
+f = open('sales_data.txt', 'r')
 print(f.read())
 f.close()
 
-f = open('sales_date.txt', 'w')
+f = open('sales_data.txt', 'w')
 f.write(', '.join(str(sale) for sale in sales))
 f.close()
 
-f = open('sales_date.txt', 'r')
+f = open('sales_data.txt', 'r')
 print("Comma seperated data")
 print(f.read())
 f.close()
--- Assignment4/task2.py
+++ Assignment4/task2.py
@@ -1,22 +1,22 @@
 sales = [1200, 450, 980, 1500, 3000]
-f = open('sales_date.txt', 'w')
+f = open('sales_data.txt', 'w')
 for sale in sales:
     f.write(f'{str(sale)}\n')
 f.close()
 
-f = open('sales_date.txt', 'r')
+f = open('sales_data.txt', 'r')
 print("Read entire file")
 print(f.read())
 f.close()
 
 print("-"*50)
-f = open('sales_date.txt', 'r')
+f = open('sales_data.txt', 'r')
 print("Read first line")
 print(f.readline())
 f.close()
 
-~print("-"*50)
-f = open('sales_date.txt', 'r')
+print("-"*50)
+f = open('sales_data.txt', 'r')
 print("Read all line using readlines")
 line = [line.strip() for line in f.readlines()]
 print(line)
