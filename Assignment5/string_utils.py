e9331e153f83c2d5123d3b658db8119a76684b33
Nishant Santosh Jadhav <nishantjadhav8105@gmail.com>
2026-03-21 21:09:35 +0530

    Create module with function Capitalize_word, reverse_string, word_count

---
+++ Assignment5/string_utils.py
@@ -0,0 +1,13 @@
+def capitalize_words(text):
+    cap_text = text.upper()
+    return cap_text
+
+def reverse_string(text): #nishant
+    reverse = ''
+    for i in text :
+        reverse = i +reverse
+
+    return reverse
+
+def word_count(text):
+    return len(text)
