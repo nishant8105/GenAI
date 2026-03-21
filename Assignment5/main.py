7c9a0a413a63df19e4b8c9a348cba48de34bd330
Nishant Santosh Jadhav <nishantjadhav8105@gmail.com>
2026-03-21 21:08:12 +0530

    use string_utils module

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -4,3 +4,14 @@ from math_utils import square
 print(f"Addition of 5 and 6 is {mu.add(5, 6)}")
 print(f"subtraction of 18 and 3 is {mu.subtract(5, 6)}")
 print(f"Square of 9 is {square(9)}")
+print()
+print("="*25)
+import string_utils as su
+print()
+print(f"Capitalize word of 'nishant' is {su.capitalize_words('nishant')}")
+print(f"Reverse of 'nishant' is {su.reverse_string('nishant')}")
+print(f"Words count in 'nishant' is {su.word_count('nishant')}")
+
+print()
+print("="*25)
+print()
