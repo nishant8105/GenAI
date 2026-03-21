528215ac3ad7361182e238b88ed9bf08b0da8684
Nishant Santosh Jadhav <nishantjadhav8105@gmail.com>
2026-03-21 20:54:24 +0530

    use math_utils module

---
+++ Assignment5/main.py
@@ -0,0 +1,6 @@
+import math_utils as mu
+from math_utils import square
+
+print(f"Addition of 5 and 6 is {mu.add(5, 6)}")
+print(f"subtraction of 18 and 3 is {mu.subtract(5, 6)}")
+print(f"Square of 9 is {square(9)}")
