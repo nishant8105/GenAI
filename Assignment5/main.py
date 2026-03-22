a7414abab3306c1df4f8394b861a346029af7959
Nishant Santosh Jadhav <nishantjadhav8105@gmail.com>
2026-03-22 16:31:18 +0530

    Fix subtraction argument in main.py

--- Assignment5/main.py
+++ Assignment5/main.py
@@ -2,7 +2,7 @@ import math_utils as mu
 from math_utils import square
 
 print(f"Addition of 5 and 6 is {mu.add(5, 6)}")
-print(f"subtraction of 18 and 3 is {mu.subtract(5, 6)}")
+print(f"subtraction of 18 and 3 is {mu.subtract(18, 3)}")
 print(f"Square of 9 is {square(9)}")
 print()
 print("="*25)
@@ -15,3 +15,9 @@ print(f"Words count in 'nishant' is {su.word_count('nishant')}")
 print()
 print("="*25)
 print()
+
+import shop_package.discount as disc
+from shop_package.billing import calculate_total
+
+print(disc.apply_discount(1000, 10))
+print(calculate_total([100, 200, 300]))
