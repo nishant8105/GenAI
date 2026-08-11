ad24d2b3831737abbb28bcbbd19969d793f096c6
nishant8105 <nishantjadhav8105@gmail.com>
2026-08-11 20:17:45 +0530

    Square root, exponential, natural log, sun of all element, Cumulatiove sum of all element

---
+++ Assignment9/numpy_task3.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+values = np.array([2, 4, 6, 8, 10])
+
+print("Square root: ",np.sqrt(values))
+print("Exponential: ",np.exp(values))
+print("Natural Logarithm: ",np.log(values))
+print("Sum of all element: ",np.sum(values))
+print("Cumulative sum of element: ",np.cumsum(values))
