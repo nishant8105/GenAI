23b3e1f1d4500b9e4619d40a214bcbe6687d492b
nishant8105 <nishantjadhav8105@gmail.com>
2026-08-11 20:16:11 +0530

    Arithematic operation on Matrix

---
+++ Assignment9/numpy_task2.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+A = np.array([10, 20, 30, 40])
+B = np.array([1, 2, 3, 4])
+
+
+print(f"{A + B = }")
+print("Addition: ", np.add(A, B))
+print(f"{A - B = }")
+print("Subtraction: ", np.subtract(A, B))
+print(f"{A * B = }")
+print(f"{A / B = }")
+print(f"{A ** 2 = }")
