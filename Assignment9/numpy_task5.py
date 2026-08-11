34425bb1e9ee26ed502ede34fd03b2ea1a99658a
nishant8105 <nishantjadhav8105@gmail.com>
2026-08-11 20:19:19 +0530

    mean, median, variance, std, range

---
+++ Assignment9/numpy_task5.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+marks = np.array([78, 85, 90, 66, 72, 88, 95, 60])
+
+print("Mean: ", np.mean(marks))
+print("Median: ", np.median(marks))
+print("Variance: ", np.var(marks))
+print("Standard Deviation: ", np.std(marks))
+print(f"Minimum : {np.min(marks)} and maximum value: {np.max(marks)}", )
+print("Range: ",(np.max(marks) - np.min(marks)))
